d81d83f9b4e8f76dbbda57da341f7a314d9158fe
Gunnell <gnl0200@gmail.com>
2023-10-05 15:38:52 +0300

    var a

--- OnlineLearningPlatform/Program.cs
+++ OnlineLearningPlatform/Program.cs
@@ -11,7 +11,8 @@ if (!app.Environment.IsDevelopment())
 {
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
-	var a = 6;
+	var a = 4;
+	
 }
 
 app.UseHttpsRedirection();
