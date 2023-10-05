b0e8b8cdc980c13837c69dfd048f045bc463515f
Doğukan Karabıyık <dokarabiyik2008@gmail.com>
2023-10-05 18:14:35 +0300

    -Created Sqlite database -Added connection string -Added auth -Added data context

--- OnlineLearningPlatform/Program.cs
+++ OnlineLearningPlatform/Program.cs
@@ -1,8 +1,11 @@
+using OnlineLearningPlatform.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -11,13 +14,14 @@ if (!app.Environment.IsDevelopment())
 {
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
-	var a = 10;
-	
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 
 
 app.MapControllerRoute(
