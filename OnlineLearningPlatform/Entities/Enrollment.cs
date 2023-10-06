86225d1001a04c13ae5d9df8f87ae7ccff232174
Doğukan Karabıyık <dokarabiyik2008@gmail.com>
2023-10-06 13:46:32 +0300

    -Added new Entities

---
+++ OnlineLearningPlatform/Entities/Enrollment.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineLearningPlatform.Entities
+{
+	[Table("Enrollments")]
+	public class Enrollment
+	{
+		public int Id { get; set; }
+		public DateTime EnrollmentDate { get; set; }
+
+		//relationship properties
+		public int CourseId { get; set; }
+		public Course Course { get; set; }
+		public int AppUserId { get; set; }
+		public AppUser AppUser { get; set; }
+	}
+}
