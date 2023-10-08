f9321efa48bcc12af2dcd1f45ffddb1f0e8d45b8
Doğukan Karabıyık <dokarabiyik2008@gmail.com>
2023-10-08 20:27:22 +0300

    -Added course update DTO -Added member info DTO

---
+++ OnlineLearningPlatform/DTOs/CourseUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineLearningPlatform.DTOs
+{
+	public class CourseUpdateDto
+	{
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string Category { get; set; }
+		public string ImageUrl { get; set; }
+	}
+}
