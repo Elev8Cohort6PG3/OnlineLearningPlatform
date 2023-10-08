25448768fbf034915a0ad3f775d80d65373c9f12
Doğukan Karabıyık <dokarabiyik2008@gmail.com>
2023-10-08 18:56:38 +0300

    Added nullable for first name and last name

--- OnlineLearningPlatform/Entities/AppUser.cs
+++ OnlineLearningPlatform/Entities/AppUser.cs
@@ -4,8 +4,8 @@ namespace OnlineLearningPlatform.Entities
 {
 	public class AppUser : IdentityUser<int>
 	{
-		public string FirstName { get; set; }
-		public string LastName { get; set; }
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
 
 		public List<AppUserRole> UserRoles { get; set; }
 	}
