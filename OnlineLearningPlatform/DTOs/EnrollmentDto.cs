e6a93e06291d8a5d32cc0b38021632c62f7306dd
Doğukan Karabıyık <dokarabiyik2008@gmail.com>
2023-10-10 22:19:14 +0300

    -Modified enrollment DTO -Modified queries in enrollment repository

--- OnlineLearningPlatform/DTOs/EnrollmentDto.cs
+++ OnlineLearningPlatform/DTOs/EnrollmentDto.cs
@@ -5,5 +5,7 @@
 		public int Id { get; set; }
 		public DateTime EnrollmentDate { get; set; }
 		public double CompletionPercentage { get; set; }
+		public int CourseId { get; set; }
+		public int AppUserId { get; set; }
 	}
 }
